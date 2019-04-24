0d44896c842b6045a2e911a09c9871105a83ab8d
lzh960909 <945963532@qq.com>
2019-04-24 13:37:47 +0800

    4.24

---
+++ preProcess.py
@@ -0,0 +1,19 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt  # Matlab-style plotting
+import seaborn as sns
+color = sns.color_palette()
+sns.set_style('darkgrid')
+import warnings
+import xgboost
+def ignore_warn(*args, **kwargs):
+    pass
+warnings.warn = ignore_warn #ignore annoying warning (from sklearn and seaborn)
+
+from scipy import stats
+from scipy.stats import norm, skew #for some statistics
+
+pd.set_option('display.float_format', lambda x: '{:.3f}'.format(x)) #Limiting floats output to 3 decimal points
+
+from subprocess import check_output
+print(check_output("test.csv").decode("utf8")) #check the files available in the directory
